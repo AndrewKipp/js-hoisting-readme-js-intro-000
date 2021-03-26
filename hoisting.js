0d283e642c9e977dfee708fb7aba29f46b048eaa
Andrew Kipp <andrewkipp1@gmail.com>
2021-03-26 19:29:44 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -1,38 +1,27 @@
-var lyric;
-lyric = "maybe";
-
 function callMe() {
-  return lyric;
-  console.log("maybe");
-}
-
-crazy();
-
-var lyric;
-lyric = "I just met you..."
-
-function crazy(); {
-  return lyric;
+  var lyric = "maybe";
   console.log("I just met you...");
+  console.log("and this is crazy..");
+  console.log("but here's my number..");
+  console.log("so call me");
+  return lyric;
 }
 
-thisIsCrazy();
 
-thisIsCrazy = function() {
-  console.log("hey!!!");
-}
-
+function crazy() {
+  // fix the code in here:
+  var thisIsCrazy = function (){
+    console.log("hey!!!")
   }
+  thisIsCrazy();
 }
 
 function sayMyName() {
   // fix the code in here:
   var name = "Cricky";
-
-  sayMy();
-
   function sayMy() {
-    console.log(name);
     var name = "Kristin";
+    console.log(name);
   }
+  sayMy();
 }
